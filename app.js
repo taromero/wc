68dc139644d0b18a7c90fef9511d95d139f21b4a
Tomas Romero <canotto90@gmail.com>
2014-03-08 20:26:55 -0300

    pretty-error

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-// Start sails and pass it command line arguments
-require('sails').lift(require('optimist').argv);
+var local_cfg = require('./config/local')
+
+if(local_cfg.log_level == 'debug') {
+  // Start sails and pass it command line arguments
+  require('sails').lift();
+} else {
+  var pe = require('pretty-error').start(function(){
+    require('sails').lift();
+  })
+
+  pe.skipPackage('sails');
+  pe.alias('/home/canotto90/proyectos/wc', 'wc');
+}
