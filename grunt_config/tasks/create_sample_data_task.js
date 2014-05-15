9e3f47fed1301924af684a21ef7d90b39d9ab605
Tomas Romero <canotto90@gmail.com>
2014-05-14 22:49:52 -0300

    fix data creation task

--- grunt_config/tasks/create_sample_data_task.js
+++ grunt_config/tasks/create_sample_data_task.js
@@ -1,13 +1,38 @@
 var q = require('q')
 
+var sampleUser = (function() {
+  var counter = 0
+  return function() {
+    counter++
+    return {
+      email: 'canotto90' + counter + '@gmail.com',
+      password: '123',
+      name: 'Tomas',
+      surname: 'Romero',
+      gender: 'Male',
+      address: 'A Direction 123',
+      phone: '1234-5678',
+      role: 'STUDENT'
+    }
+  }
+})()
+
+function customUser(customMap) {
+  var sample_user = sampleUser()
+  for(var key in customMap) {
+    sample_user[key] = customMap[key]
+  }
+  return sample_user
+}
+
 module.exports = function(grunt) {
   grunt.registerTask('create_sample_data', 'create sample data', function() {
     var Sails = require('sails');
     var done = this.async();
     Sails.lift({ log: { level: 'error' } }, function(err, sails) {
       q.all([
-        User.create({ email: 'canotto90@gmail.com', password: '123', role: 'STUDENT' }),
-        User.create({ email: 'canotto90+1@gmail.com', password: '123', role: 'PROFESSOR' })
+        User.create(sampleUser()),
+        User.create(customUser({role: 'ADMIN'}))
       ])
       .then(console.log)
       .fail(console.log)
